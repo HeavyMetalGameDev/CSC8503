b669bc04d7c7ee3a73ea382fcd967c81e72c082b
HeavyMetalGameDev <Matthewryan987@gmail.com>
2023-11-30 14:46:11 +0000

    Completed first Networking tutorial.

--- CSC8503/Main.cpp
+++ CSC8503/Main.cpp
@@ -242,6 +242,55 @@ void TestBehaviourTree() {
 	std::cout << "All Done!\n";
 }
 
+class TestPacketReceiver :public PacketReceiver {
+public: 
+	TestPacketReceiver(std::string name) {
+		this->name = name;
+	}
+
+	void ReceivePacket(int type, GamePacket* payload, int source) {
+		if (type == String_Message) {
+			StringPacket* realPacket = (StringPacket*) payload;
+			std::string msg = realPacket->GetStringFromData();
+
+			std::cout << name << " recieved message " << msg << "\n";
+		}
+	}
+protected:
+	std::string name;
+};
+
+void TestNetworking(){
+	NetworkBase::Initialise();
+
+	TestPacketReceiver serverReceiver("Server");
+	TestPacketReceiver clientReceiver("Client");
+
+	int port = NetworkBase::GetDefaultPort();
+
+	GameServer* server = new GameServer(port, 1);
+	GameClient* client = new GameClient();
+
+	server->RegisterPacketHandler(String_Message, &serverReceiver);
+	client->RegisterPacketHandler(String_Message, &clientReceiver);
+
+	bool canConnect = client->Connect(127, 0, 0, 1, port);
+
+	for (int i = 0; i < 100; i++) {
+		StringPacket pServer("Server says hello! " + std::to_string(i));
+		server->SendGlobalPacket(pServer);
+
+		StringPacket pClient("Client says hello! " + std::to_string(i));
+		client->SendPacket(pClient);
+
+		server->UpdateServer();
+		client->UpdateClient();
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+	NetworkBase::Destroy();
+}
+
 /*
 
 The main function should look pretty familar to you!
@@ -257,7 +306,8 @@ hide or show the
 int main() {
 	Window*w = Window::CreateGameWindow("CSC8503 Game technology!", 1280, 720);
 
-	TestPushDownAutomata(w);
+	//TestPushDownAutomata(w);
+	TestNetworking();
 	if (!w->HasInitialised()) {
 		return -1;
 	}	
